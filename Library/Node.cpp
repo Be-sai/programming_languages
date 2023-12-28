f19c363a757b407bdb1fc48797dfbb724be14219
Be-sai <152933507+Be-sai@users.noreply.github.com>
2023-12-28 14:50:34 +0300

    Update Node.cpp

--- Library/Node.cpp
+++ Library/Node.cpp
@@ -1,8 +1,3 @@
 #include "Node.h"
 
-/**
- * @brief Конструктор узла.
- * @param value Значение узла.
- * @param next Указатель на следующий узел (по умолчанию nullptr).
- */
 Node::Node(size_t value, Node* next) : element(value), pNext(next) {}
